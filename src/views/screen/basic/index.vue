0dc414c7abb476e7b058f6b1ac5ec0bfcacc09ef
YwonDong <futurecreaterlan@foxmail.com>
2024-07-09 09:16:29 +0800

    pages

--- src/views/screen/basic/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import Weather from '../Charts/components/Weather.vue'
-import InfoBanner from '../Charts/components/InfoBanner.vue'
-</script>
-
-<template>
-  <Weather></Weather>
-  <InfoBanner></InfoBanner>
-</template>
