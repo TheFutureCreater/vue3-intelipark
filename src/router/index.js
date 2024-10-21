292e0caa7ade586fe67147ec5d613cd06c2a8c0f
YwonDong <futurecreaterlan@foxmail.com>
2024-10-21 22:20:49 +0800

    0000

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 // import { useUserStore } from '@/stores'
 
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: () => import('@/views/login/LoginPage.vue') },
