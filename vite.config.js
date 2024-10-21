2b0a9f118f7235385885a7a6b41380238f6bacdd
YwonDong <futurecreaterlan@foxmail.com>
2024-10-21 21:02:26 +0800

    ghPages

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: './',
+  base: '/vue3-intelipark/', // github仓库名称
   plugins: [
     vue(),
     AutoImport({
